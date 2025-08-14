8bd2ac0249ce63f6968fd2b74a0c6dd8471cd962
Prabhgun Singh <godsent@Prabhguns-MacBook-Air.local>
2025-08-14 15:20:08 +0530

    Lec 18

---
+++ Lec-17/model/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name : String,
+    email: String ,
+    password: String , 
+    blogs : [
+        {
+            type :Schema.Types.ObjectId ,
+            ref : "Blogs"
+        }
+    ]
+          
+});
+
+module.exports = mongoose.model('User', UserSchema);
